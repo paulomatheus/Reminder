de65e52b98c433ab9d758652ad10931af2026fb0
paulomatheus <paulomatheus.araujo@hotmail.com>
2026-03-13 19:57:36 -0300

    feat: add Firebase Auth

--- Reminder/Sources/Features/LoginBottonSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottonSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -11,5 +11,13 @@ import Firebase
 class LoginBottomSheetViewModel {
     func doAuth(usernameLogin: String, password: String){
         print(usernameLogin)
+        Auth.auth().signIn(withEmail: usernameLogin, password: password){ [weak self] authResult, error in
+            if let error = error {
+                print("Error signing in: \(error.localizedDescription)")
+            } else {
+                print(authResult, "User signed in successfully")
+            }
+        }
+            
     }
 }
